ce0270f0138f122b83dfcd8bc8d6ff157d893289
antoniacatarama <antonia.catarama@s.unibuc.ro>
2023-02-12 16:47:25 +0200

    final project

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {FirebaseService} from "../services/firebase.service";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
 
+  isLoggedIn!: boolean;
+  constructor(
+    private router: Router,
+    public firebaseService: FirebaseService
+  ) {
+  }
+
+ngOnInit() {
+  this.isLoggedIn = localStorage.getItem('user') !== undefined;
+}
+
+goToLogin() {
+  this.router.navigate(['login'])
+}
+
+goToRegister() {
+  this.router.navigate(['register'])
+}
+
+logout() {
+  this.isLoggedIn = false;
+  this.firebaseService.logout();
+}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./auth/login/login.component";
 import {RegisterComponent} from "./auth/register/register.component";
 import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
+  {path: 'home', component: HomeComponent },
   {path: 'about', component: AboutComponent },
+  {path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
